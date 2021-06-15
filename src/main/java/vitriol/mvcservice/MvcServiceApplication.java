867fae7653bbcee89ae95fdb3460ddf760988c86
vitriol <vitriol95@naver.com>
2021-06-15 19:20:53 +0900

    firstinit

---
+++ src/main/java/vitriol/mvcservice/MvcServiceApplication.java
@@ -0,0 +1,13 @@
+package vitriol.mvcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vitriol/mvcservice/MvcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vitriol.mvcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
